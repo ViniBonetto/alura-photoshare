bc30f067b8f31025201e1a5f569042765580eb6b
Vinícius Bonetto <vini.bonetto10@gmail.com>
2021-09-17 14:59:37 -0300

    Adicionando validação no form do login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,3 +1,4 @@
+import { MensagemModule } from './../componentes/mensagem/mensagem.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -16,6 +17,7 @@ import { FormsModule } from '@angular/forms';
     CommonModule,
     HomeRoutingModule,
     FormsModule,
+    MensagemModule,
   ],
   exports: [
     HomeComponent
